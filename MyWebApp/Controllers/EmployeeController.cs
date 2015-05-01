ef48889d137b28021e60a40685bb0d99a29c7019
1101137245 <1101137245@gm.kuas.edu.tw>
2015-05-01 17:34:30 +0800

    V1.0

---
+++ MyWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebApp.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+
+        [HttpGet]
+        [ActionName("byId")]
+        public Employee GetEmployeeById(string id)
+        {
+            var employee = EmployeeService.GetEmployeeById(id);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+    }
+}
